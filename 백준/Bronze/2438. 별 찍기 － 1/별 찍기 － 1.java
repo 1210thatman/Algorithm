6dddbd29a5988afc123fe42c70fd0b33e6a883b0
최선재 <163826678+1210thatman@users.noreply.github.com>
2024-04-16 18:04:05 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 304 ms, Memory: 18120 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner  input = new Scanner(System.in);
+        int star = input.nextInt();
+        for(int i=0; i<star; i++) {
+            for(int j=0; j<i+1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
