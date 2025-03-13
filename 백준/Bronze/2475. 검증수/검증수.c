6d77d1597277ce02ba4c7372ca37ee072c98d6be
최선재 <163826678+1210thatman@users.noreply.github.com>
2025-03-13 19:53:35 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.c
+++ 백준/Bronze/2475. 검증수/검증수.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-	int nums[6] = { 0 };
 	int sum = 0;
-	for (int i = 0; i < 6; i++) {
-		scanf("%d", &nums[i]);
-		sum += nums[i] * nums[i];
+    int n = 0;
+
+	for (int i = 0; i < 5; i++) {
+		scanf("%d", &n);
+		sum += n * n;
 	}
-
-	// for (int i = 0; i < 6; i++) {
-	// 	printf("%d ", nums[i]);
-	// }
-
-	printf("%d", sum%10);
-	return 0;
-}
+	printf("%d", sum % 10);
+}//좀 더 깔끔한 코드
